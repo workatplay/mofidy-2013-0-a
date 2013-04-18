9e10dfe9c696bf4ff1b6eb5e79c3142b0de3438b
Greg Field <greg.field@workatplay.com>
2013-04-18 15:14:33 -0700

    Cleaning up the main.js file, removing unwanted code

--- zapp/assets/www/js/main.js
+++ zapp/assets/www/js/main.js
@@ -1,27 +1,18 @@
-//serverUrl = window.location.href + 'server.php?_url=';
-//if (navigator.userAgent.indexOf('GoogleTV')>0 || navigator.userAgent.indexOf('Android')>0) {
-  serverUrl = "http://tvhackfest.workatplay.com/server.php?_url=";
-//}
+serverUrl = "http://tvhackfest.workatplay.com/server.php?_url=";
 
 ready = false;
 
 window.result = function(callback) {
   if (cordova && cordova.exec) {
-    cordova.exec(callback, $.noop, "Appplause", "result", []);
+    cordova.exec(callback, $.noop, "ZappTV", "result", []);
   }
 };
 
 var resultObject;
 // Default result values
-var contentId = "c04a2c84-6e8d-11e2-a9cd-fa163e53d66f";
-var programTitle = "the_simpsons_s24_e9";
+var contentId = "5a7780f2-a676-11e2-ae21-22000af492d5";
+var programTitle = "Dragons' Den Season 7, Episode 12";
 var referenceOffset = 0;
-var ytids = ['5JO6tq-bCkc', 'MiBkjXOdWYY'];
-var _index = Math.round(Math.random()*ytids.length)%ytids.length;
-var ytid = ytids[_index];
-if (ytid == '5JO6tq-bCkc') {
-  programTitle = "30_rock_s7_e11";
-}
 
 var seconds = '0';
 var data = {
@@ -35,34 +26,39 @@ var maxClaps = 0;
 
 function onDeviceReady() {
   ready = true;
-
   var myShowIsSet = false;
 
-  window.result(function(jsonString) {
-    // alert(jsonString);
+  window.result(function (jsonString) {
     resultObject = jQuery.parseJSON(jsonString);
 
     if (resultObject.content_id) {
       window.contentId = resultObject.content_id;
     }
-    
-//    5JO6tq-bCkc
-    
-    for(var i = 0; i < resultObject.content_attrs.length; i++) {
-      var pair = resultObject.content_attrs[i];
-      if (pair.name == "program_title" && pair.value) {
-        window.programTitle = pair.value;
-        $('#myshow').text(programTitle);
-        comments.init();
-        myShowIsSet = true;
-      } else if (pair.name == "reference_offset" && pair.value) {
-        window.referenceOffset = parseInt(pair.value[0], 10);
+
+    _.each(resultObject.content_attrs, function (attr) {
+      if (attr.name === 'program_title') {
+        window.programTitle = attr.value;
+      } else if (attr.name === 'reference_offset') {
+        var offet = parseInt(pair.value[0], 10);
         window.referenceOffset = Math.round(window.referenceOffset / 5) * 5;
       }
-    }
+    });
 
-    seconds = referenceOffset.toString();
-    comments.showTime = referenceOffset;
+    window.seconds = referenceOffset.toString();
+//    comments.showTime = referenceOffset;
+
+    // for (var i = 0; i < resultObject.content_attrs.length; i++) {
+    //   var pair = resultObject.content_attrs[i];
+    //   if (pair.name == "program_title" && pair.value) {
+    //     window.programTitle = pair.value;
+    //     $('#myshow').text(programTitle);
+    //     comments.init();
+    //     myShowIsSet = true;
+    //   } else if (pair.name == "reference_offset" && pair.value) {
+    //     window.referenceOffset = parseInt(pair.value[0], 10);
+    //     window.referenceOffset = Math.round(window.referenceOffset / 5) * 5;
+    //   }
+    // }
   });
 
   setTimeout(function() {
@@ -83,43 +79,6 @@ window.onload = function () {
 // If PhoneGap, run when ready.
 document.addEventListener('deviceready', onDeviceReady, false);
 
-var plause = $('#plause');
-plause.css({ opacity: '0.3' });
-
-// Clapping
-$('body').on('click', function() {
-  // console.log('clap', data[seconds].claps++);
-  data[seconds].claps++;
-  claps[claps.length - 1]++;
-  if (maxClaps < claps[claps.length - 1]) {
-    maxClaps = claps[claps.length - 1];
-    plause.css({ opacity: '1' });
-  }
-});
 setInterval(function() {
-  var lastClaps = 0;
-  if (data[seconds] && data[seconds].claps) {
-    lastClaps = data[seconds].claps;
-  }
-
-  console.log('claps', lastClaps);
-
   seconds = (parseInt(seconds, 10) + 5).toString();
-  // TODO: Extend previous data
-  data[seconds] = {
-    claps: 0,
-    comments: []
-  };
-  if (clientMode > 1) {
-    claps.shift();
-    claps.push(_.random(0, 14));
-  } else {
-    claps = _.last(_.pluck(data, 'claps'), 10); // TODO: Use global points var
-  }
-  while (claps.length < 10) {
-    claps.unshift(0);
-  }
-  maxClaps = _.max(claps);
-
-  plause.css({ opacity: 0.3 + (lastClaps / maxClaps) * 0.7 });
 }, 5000);

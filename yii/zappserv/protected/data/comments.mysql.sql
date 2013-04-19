ea62471ede9d7b6a392d83e4fe4cad61b7a203d0
Greg Field <greg.field@workatplay.com>
2013-04-18 22:50:11 -0700

    comments to populate in db

---
+++ yii/zappserv/protected/data/comments.mysql.sql
@@ -0,0 +1,39 @@
+INSERT INTO `comments`
+(`time`,`user`,`video`,`created`,`data`)
+VALUES (589,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"I could use a drink about now!","position":"top-right"}'),
+(599,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Better with vodka.","position":"bottom"}'),
+(610,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Vodka, would be better.","position":"top-left"}'),
+(620,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Where are the boys?","position":"top-right"}'),
+(629,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Still no vodka?","position":"top-left"}'),
+(642,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Finally Vodka!","position":"top-left"}'),
+(681,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Too much vodka!!!","position":"top-left"}'),
+(714,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Our ice is … smaller.","position":"top-right"}'),
+(719,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Seriously. It is smaller.","position":"top-right"}'),
+(725,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Small ice is important, you know.","position":"top-right"}'),
+(732,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Clear bags. There\'s a business.","position":"bottom"}'),
+(738,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Yeah! Blue bags are POP, y\'all!","position":"bottom"}'),
+(743,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Are you F$!@ing kidding me?","position":"top-right"}'),
+(750,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Now she\'s shitting me.","position":"top-right"}'),
+(757,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"No, she\'s right. Need ice at a BBQ.","position":"bottom"}'),
+(767,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Sweeeeeet…. Emoooo…shun…","position":"bottom"}'),
+(776,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Bazinga!","position":"top-right"}'),
+(799,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Jinx!!","position":"top-left"}'),
+(805,'Billy','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Double Jinx!!!","position":"top-left"}');
+
+INSERT INTO `comments`
+(`time`,`user`,`video`,`created`,`data`)
+VALUES (568,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Jennifer Trayler - Previous job: Esthetician Danielle Nesbitt","position":"top-right"}'),
+(573,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Vancouver is Canada\'s third largest city for start-ups","position":"bottom"}'),
+(582,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Bruce: For that valuation I\'d expect a young company with revenues between $200,000 and $400,000","position":"bottom"}'),
+(588,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Kevin: We always like when presenters give us samples","position":"top-left"}'),
+(598,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Arlene: Didn\'t enjoy the warm tea.","position":"top-left"}'),
+(609,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Jim: It\'s fun to play and banter with entrepreneuers.","position":"top-right"}'),
+(622,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Arlene: I think presenters make a mistake presenting female models. They don\'t excite me, and I\'m the shows biggest investor.","position":"top-right"}'),
+(635,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Arlene: I\'m always interested in new brands in an old unchanging industry. That\'s where you find opportunity.","position":"top-right"}'),
+(645,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Ice Sphere\'s were a Japanese innovation at boutique whiskey bars in the 1990s","position":"bottom"}'),
+(651,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Bruce: I enjoy a drink but I wasn\'t keen on the straight vodka.","position":"top-right"}'),
+(676,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"On The Rocks Ice: started in business doing Ice Sculpture for local bars.","position":"bottom"}'),
+(684,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Kevin: I hated this business as soon as I heard it was about selling ice. Selling commodities is a hard business.","position":"top-right"}'),
+(691,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"The ice trade started in the 1700s, with harvesting of natural ice from the Alps and Himalayas","position":"bottom"}'),
+(700,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Canada is the World\'s second largest producer of ice cubes.","position":"bottom"}'),
+(855,'Official','Dragons\' Den Season 7, Episode 12', now(), '{"msg":"Arctic Glalcier filed for Bankruptcy February 2012","position":"bottom"}');
